aa6edda0a2207ef70c0bc6db0f8b736bbc77c930
Limidesk <ddonng@Limidesks-MBP.local>
2016-08-01 00:38:27 +0800

    static

--- src/Library.php
+++ src/Library.php
@@ -7,20 +7,20 @@ class Library
 
     public function __construct($config)
     {
-        $this->client = new \DoraRPC\Client($config);
+        self::client = new \DoraRPC\Client($config);
     }
 
     private function coreGroup()
     {
         $mode = array("type" => 1, "group" => "core_group");
-        $this->client->changeMode($mode);
+        self::client->changeMode($mode);
     }
 
 
     public function checkLoginName($lname)
     {
         $this->coreGroup();
-        $result = $this->client->singleAPI("checkLoginName",array("login_type"=>"email","login_name"=>"ddonng@qq.com"),
+        $result = self::client->singleAPI("checkLoginName",array("login_type"=>"email","login_name"=>"ddonng@qq.com"),
             \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1);
         return $result;
     }
