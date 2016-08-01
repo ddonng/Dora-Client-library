6d703411e425d6546aebf85ee37770985aacc5f1
Limidesk <ddonng@Limidesks-MBP.local>
2016-08-01 21:47:13 +0800

    core-group

--- src/Library.php
+++ src/Library.php
@@ -10,19 +10,108 @@ class Library
         self::$client = new \DoraRPC\Client($config);
     }
 
-    private function coreGroup()
+
+    private function extract_res($result)
     {
-        $mode = array("type" => 1, "group" => "core_group");
+
+        return $result['data']['data']['data'];
+    }
+
+    private function setGroup($group)
+    {
+        $mode = array("type" => 1, "group" => $group);
         self::$client->changeMode($mode);
     }
 
 
-    public function checkLoginName($lname)
+    public function checkLoginName($loginType,$loginName)
     {
-        $this->coreGroup();
-        $result = self::$client->singleAPI("checkLoginName",array("login_type"=>"email","login_name"=>"ddonng@qq.com"),
+        $this->setGroup("core_group");
+        $result = self::$client->singleAPI(
+            "checkLoginName",
+            array("login_type"=>$loginType,"login_name"=>$loginName),
             \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1);
-        return $result;
+        return extract_res($result);
     }
+
+    public function checkUserLoginInfo($loginType,$loginName,$passwd)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "checkUserLoginInfo",
+            array("loginType"=>$loginType,"loginName"=>$loginName,"password"=>$passwd),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1);
+        return extract_res($result);
+    }
+
+    public function getUserById($userId)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "getUserById",
+            array("user_id"=>$userId),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1
+        );
+
+        return extract_res($result);
+
+    }
+
+    public function addUser($user)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "addUser",
+            array("user"=>$user),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1
+        );
+
+        return extract_res($result);
+    }
+
+    public function updateUser($user)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "updateUser",
+            array("user"=>$user),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1
+        );
+
+        return extract_res($result);
+
+    }
+
+    public function deleteUser($userId)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "deleteUser",
+            array("user_id"=>$userId),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1
+        );
+        return extract_res($result);
+    }
+
+    public function getUsers($fields)
+    {
+        $this->setGroup("core_group");
+
+        $result = self::$client->singleAPI(
+            "getUsers",
+            array("fields"=>$fields),
+            \DoraRPC\DoraConst::SW_MODE_WAITRESULT, 1
+        );
+
+        return extract_res($result);
+    }
+
+
+
 
 }
